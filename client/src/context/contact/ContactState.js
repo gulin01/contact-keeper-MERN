3f2d40b1ce883ea581074db4e0f95c9eaed3a47b
gulin01 <gulin9717@gmail.com>
2020-12-23 10:39:35 +0900

    contact form component and methods

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import {v4 as uuid} from "uuid"; 
+
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 
@@ -38,20 +39,46 @@ const ContactState = props =>{
                 type: "personal"
             }
 
-        ]
+        ],
+        current:null
       
     };
     const [state,dispatch] = useReducer(contactReducer,initialState);
 
     // Add contact
 
-    // Delete contact
+    const addContact = contact =>{
+        // gives a random id
+        contact.id = uuid();
+        dispatch({type:ADD_CONTACT,payload: contact });
 
+    }
+
+    // Delete contact
+    const deleteContact = id =>{
+        dispatch({ type:DELETE_CONTACT, payload: id });
+
+    }
+
+ 
     // Set current Contact
+    const setCurrent = contact =>{
+        dispatch({ type: SET_CURRENT, payload: contact });
+
+    }
+
 
     // clear current contact
+    const clearCurrent = () =>{
+        dispatch({ type :CLEAR_CURRENT });
+
+    }
 
     // Update contact
+    const updateContact = contact =>{
+        dispatch({ type: UPDATE_CONTACT, payload: contact });
+
+    }
 
     // Filter contacts
 
@@ -60,7 +87,14 @@ const ContactState = props =>{
     return (
         <ContactContext.Provider
         value={{
-            contacts: state.contacts
+            contacts: state.contacts,
+            current:state.current,
+            addContact,
+            deleteContact,
+            setCurrent,
+            clearCurrent,
+            updateContact
+            
         }}
         >
             { props.children }
